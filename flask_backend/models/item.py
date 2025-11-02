c0f005ae886bc865a4fec5239f6c4a8742d4e69c
Magnus Jonsson <fot@fot.nu>
2025-11-02 21:16:54 +0100

    Docs!

--- flask_backend/models/item.py
+++ flask_backend/models/item.py
@@ -1,9 +1,13 @@
+"""
+Item Model
+"""
 from sqlalchemy.orm import mapped_column
 
 from flask_backend.database import db
 
 
 class Item(db.Model):
+    """ORM model for an item."""
     __tablename__ = "items"
     id = mapped_column(db.Integer, primary_key=True, index=True)
     name = mapped_column(db.String(255), index=True)
